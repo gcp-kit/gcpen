9651f4a1cf696e10337622e6e67a48c0370961ad
Yushu.Kozaki <yushu.kozaki@topgate.co.jp>
2022-05-07 02:57:43 +0900

    tests: support for Cloud Run

--- environ_test.go
+++ environ_test.go
@@ -6,15 +6,19 @@ import (
 )
 
 const (
-	testProject    = "test-project"
-	testGAEService = "test-gae-service"
-	testGAEVersion = "test-gae-version"
+	testProject     = "test-project"
+	testGAEService  = "test-gae-service"
+	testGAEVersion  = "test-gae-version"
+	testRUNService  = "test-run-service"
+	testRUNRevision = "test-run-revision"
 )
 
 func init() {
 	os.Setenv(EnvKeyGoogleCloudProject, testProject)
 	os.Setenv(EnvKeyGAEServiceName, testGAEService)
 	os.Setenv(EnvKeyGAEServiceVersion, testGAEVersion)
+	os.Setenv(EnvKeyRUNServiceName, testRUNService)
+	os.Setenv(EnvKeyRUNServiceRevision, testRUNRevision)
 }
 
 func TestEnviron(t *testing.T) {
@@ -27,5 +31,11 @@ func TestEnviron(t *testing.T) {
 	}
 	if GAEServiceVersion != testGAEVersion {
 		t.Fatalf("unexpected, expected: %s, actual: %#v", testGAEVersion, GAEServiceVersion)
+	}
+	if RUNServiceName != testRUNService {
+		t.Fatalf("unexpected, expected: %s, actual: %#v", testRUNService, RUNServiceName)
+	}
+	if RUNServiceRevision != testRUNRevision {
+		t.Fatalf("unexpected, expected: %s, actual: %#v", testRUNRevision, RUNServiceRevision)
 	}
 }
